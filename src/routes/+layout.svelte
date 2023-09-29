a3c27aa93fa99e81e8d7f257757bbcfc88e726f2
ZNLake <91035765+ZNLake@users.noreply.github.com>
2023-09-29 15:29:46 -0400

    updates

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<div class="Header" />
+
+<slot />
+
+<div class="box">
+  <p style="color: white; text-align: center;">
+    © Copyright 2023 by Zebadiah Lake
+  </p>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+  .box {
+    width: auto;
+    height: 56px;
+    border-top: 0.5px solid black;
+    background: #10071d;
+    box-sizing: border-box;
+    top: 1760px;
+    position: relative;
+  }
+  .Header {
+    width: auto;
+    height: 56px;
+    border-top: 0.5px solid black;
+    background: #10071d;
+    box-sizing: border-box;
+    top: 0%;
+    position: relative;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,67 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<div class="contact-me" style="	top: 1720px;">
+  <p
+    class="contact-me-text"
+    style="color: black; text-align: center; font-weight"
+  >
+    Contact Me!
+  </p>
+</div>
+
+<p class="working-with-me" style="top: 1585px;">
+  Interested in working with me?
+</p>
+
+<style>
+  .working-with-me {
+    width: 439px;
+    height: 35px;
+    justify-content: center;
+    color: black;
+    font-family: Source Sans Pro;
+    font-size: 33px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 36px;
+    margin: auto;
+    position: relative;
+  }
+  .contact-me {
+    width: 380px;
+    height: 51px;
+    margin: auto;
+    flex-shrink: 0;
+    position: relative;
+    border-radius: 105px;
+    background: linear-gradient(330deg, #32cdc3 47.67%, #3282c3 60.89%);
+  }
+  .contact-me-text {
+    color: black;
+    font-family: Source Sans Pro;
+    font-size: 33px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 36px;
+    padding: 7px 0;
+    justify-content: center;
+  }
+
+  .working-with-me {
+    display: flex;
+    width: 439px;
+    height: 35px;
+    flex-direction: column;
+    justify-content: center;
+    flex-shrink: 0;
+    color: #000;
+    font-family: Source Sans Pro;
+    font-size: 33px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 36px;
+  }
+</style>
